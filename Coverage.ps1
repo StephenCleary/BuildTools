6e9b87eabc076c4cd68840d74a10bf9a9579cefb
Stephen Cleary <github.ourteddybear@xoxy.net>
2016-04-15 20:49:32 -0400

    Better path management

--- Coverage.ps1
+++ Coverage.ps1
@@ -1,8 +1,14 @@
-# Set $artifactLocation and $testProjectLocation before invoking this script.
+# Expected variables:
+#   $artifactLocation - the relative path to the artifact output directory containing pdb files.
+#   $testProjectLocation - the relative path to the project that can run "dnx test".
+#   $outputLocation - the relative path where test results should be stored. This path does not have to exist.
 
 $ErrorActionPreference = "Stop"
 
 $libPath = Resolve-Path $artifactLocation
+md -Force $outputLocation
+$outputPath = Resolve-Path $outputLocation
+$outputFile = Join-Path $outputPath -childpath 'coverage.xml'
 
 Function Verify-OnlyOnePackage
 {
@@ -25,16 +31,17 @@ Try
 	cd $testProjectLocation
 
 	# Execute OpenCover with a target of "dnx test"
-	iex ((Get-ChildItem ($env:USERPROFILE + '\.dnx\packages\OpenCover'))[0].FullName + '\tools\OpenCover.Console.exe' + ' -register:user -target:"dnx.exe" -targetargs:"--lib ' + $libPath + ' test" -output:coverage.xml -skipautoprops -returntargetcode -excludebyattribute:"System.Diagnostics.DebuggerNonUserCodeAttribute" -filter:"+[Nito*]*"')
+	iex ((Get-ChildItem ($env:USERPROFILE + '\.dnx\packages\OpenCover'))[0].FullName + '\tools\OpenCover.Console.exe' + ' -register:user -target:"dnx.exe" -targetargs:"--lib ' + $libPath + ' test" -output:"' + $outputFile + '" -skipautoprops -returntargetcode -excludebyattribute:"System.Diagnostics.DebuggerNonUserCodeAttribute" -filter:"+[Nito*]*"')
 
 	# Either display or publish the results
 	If ($env:CI -eq 'True')
 	{
-		iex ((Get-ChildItem ($env:USERPROFILE + '\.dnx\packages\coveralls.io'))[0].FullName + '\tools\coveralls.net.exe' + ' --opencover coverage.xml --full-sources')
+		iex ((Get-ChildItem ($env:USERPROFILE + '\.dnx\packages\coveralls.io'))[0].FullName + '\tools\coveralls.net.exe' + ' --opencover "' + $outputFile + '" --full-sources')
 	}
 	Else
 	{
-		iex ((Get-ChildItem ($env:USERPROFILE + '\.dnx\packages\ReportGenerator'))[0].FullName + '\tools\ReportGenerator.exe -reports:coverage.xml -targetdir:.')
+		iex ((Get-ChildItem ($env:USERPROFILE + '\.dnx\packages\ReportGenerator'))[0].FullName + '\tools\ReportGenerator.exe -reports:"' + $outputFile + '" -targetdir:"' + $outputPath + '"')
+		cd $outputPath
 		./index.htm
 	}
 }
