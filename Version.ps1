3b91ded6dceccf15ca2f48e96339c1b076b47b72
Stephen Cleary <StephenCleary@users.noreply.github.com>
2017-04-14 21:20:10 -0400

    Version changes for csproj files

--- Version.ps1
+++ Version.ps1
@@ -7,23 +7,27 @@ $oldVersionPreIndex = $oldVersion.IndexOf("-");
 if ($oldVersionPreIndex -eq -1)
 {
     $shortOldVersion = $oldVersion;
+    $oldVersionSuffix = "";
 }
 else
 {
     $shortOldVersion = $oldVersion.Substring(0, $oldVersionPreIndex);
+    $oldVersionSuffix = $oldVersion.Substring($oldVersionPreIndex + 1);
 }
 
 $newVersionPreIndex = $newVersion.IndexOf("-");
 if ($newVersionPreIndex -eq -1)
 {
     $shortNewVersion = $newVersion;
+    $newVersionSuffix = "";
 }
 else
 {
     $shortNewVersion = $newVersion.Substring(0, $newVersionPreIndex);
+    $newVersionSuffix = $newVersion.Substring($newVersionPreIndex + 1);
 }
 
-$files = @(dir SharedAssemblyInfo.cs -recurse) + @(dir src/*/project.json) + @(dir *.nuspec -recurse)
+$files = @(dir SharedAssemblyInfo.cs -recurse) + @(dir src/*/*.csproj) + @(dir *.nuspec -recurse)
 
 ForEach($file in $files)
 {
@@ -36,11 +40,12 @@ ForEach($file in $files)
     }
     else
     {
-        $content = $content -replace "$oldVersion", "$newVersion"
+        $content = $content -replace "<VersionPrefix>$shortOldVersion</VersionPrefix>", "<VersionPrefix>$shortNewVersion</VersionPrefix>"
+		$content = $content -replace "<VersionSuffix>$oldVersionSuffix</VersionSuffix>", "<VersionSuffix>$newVersionSuffix</VersionSuffix>"
     }
     if ([String]::Join("`r`n", $originalContent) -ne [String]::Join("`r`n", $content))
     {
-        Set-Content -Value $content -Path $file
+        Set-Content -Value $content -Path $file -Encoding UTF8
         Write-Output "Updated $file"
     }
 }
